24dcd5543d4a390d9c43896b6dfeb8b6a1d8ef2a
gezielelyon <geziel_barbosa_hotmail.com>
2021-10-19 09:29:28 -0300

    refactor: move duplicated code to a helper

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,20 +1,15 @@
 import { IHttpRequest, IHttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missingParamError'
+import { badRequest } from '../helpers/httpHelper'
 
 export class SignUpController {
   public async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
+      return badRequest(new MissingParamError('name'))
     }
 
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
+      return badRequest(new MissingParamError('email'))
     }
   }
 }
