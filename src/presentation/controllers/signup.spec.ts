e8ce8216e84f6656abbe6f0898c1fabf5386d975
gezielelyon <geziel_barbosa_hotmail.com>
2021-10-24 08:26:04 -0300

    refactor: move sut creation to a fatory helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,13 @@
 import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missingParamError'
 
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
+
 describe('SignUp Controller', () => {
   test('Should be returned status code 400 if no name is provided', async () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
@@ -19,7 +23,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Should be returned status code 400 if no email is provided', async () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -35,7 +39,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Should be returned status code 400 if no password is provided', async () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -51,7 +55,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Should be returned status code 400 if no password confirmation is provided', async () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
