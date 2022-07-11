047646a9e07000538ef0fc63c9e996cdd3d4ac2b
Geziel Elyon <geziel_barbosa_@hotmail.com>
2022-07-10 21:32:03 -0300

    refactor: add command on jest config to ignore main layer

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,8 @@ module.exports = {
   clearMocks: true,
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
